85b115213be6523bc4aa0d85960f6c34de389ca8
Malith Ramanayake <malithramanayake@gmail.com>
2026-09-17 00:11:16 +0530

    new

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-budget-v4';
+const CACHE_NAME = 'my-budget-v5';
 const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
