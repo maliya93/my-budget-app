43930ada83780e1a08098f83b720711842e15b58
Malith Ramanayake <malithramanayake@gmail.com>
2026-09-16 22:56:16 +0530

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'my-budget-v3';
-const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'my-budget-v4';
+const FILES_TO_CACHE = ['./index.html', './manifest.json', './app-icon.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
